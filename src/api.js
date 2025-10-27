8392643377bdef4bd723dfb7a81301cfa82009d1
prateetimody <sbmpprateeti789@gmail.com>
2025-10-27 20:49:40 +0530

    Connected frontend to deployed backend

--- src/api.js
+++ src/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const BACKEND_URL = "https://creditcard-parser-backend.onrender.com/"; 
+// const BACKEND_URL = "https://creditcard-parser-backend.onrender.com"; 
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+
 
 export const parseCard = async (file) => {
   const formData = new FormData();
